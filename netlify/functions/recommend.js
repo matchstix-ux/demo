118afc1e2f42399d6071935cbda7833de1d3a521
matchstix-ux <lukens.pf@gmail.com>
2026-03-29 12:03:53 -0400

    Refactor CORS setup to be inside handler
    Moved CORS configuration inside the handler function.

--- netlify/functions/recommend.js
+++ netlify/functions/recommend.js
@@ -1,14 +1,11 @@
-const fallbackCigars = require("./fallbackCigars");
-
-const CORS = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "POST,OPTIONS",
-  "Access-Control-Allow-Headers": "content-type,authorization",
-  "Content-Type": "application/json"
-};
-
 exports.handler = async function(event) {
+  const CORS = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST,OPTIONS",
+    "Access-Control-Allow-Headers": "content-type,authorization",
+    "Content-Type": "application/json"
+  };
+
   if (event.httpMethod === "OPTIONS") {
-    return {
-      statusCode
+    return { statusCode
 
